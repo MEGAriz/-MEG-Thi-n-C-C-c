fd4c6e03ef478d87dae64771f50333fd09ef5db5
MEGAriz <nguyenvulmaobru@gmail.com>
2026-09-01 07:28:24 +0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "coffee shop", // required by Discord API, not shown in the client
         state: "stalking",     // this is what people actually see
-        type: 1,               // Custom
+        type: 3,               // Custom
       },
     ],
   },
